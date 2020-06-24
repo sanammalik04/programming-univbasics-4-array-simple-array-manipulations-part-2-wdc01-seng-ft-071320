2672a6258383898b0dcbb6e8a2259bb85efb8ded
Sanam Malik <sanam.malik04@gmail.com>
2020-06-24 23:31:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,21 @@ end
 def using_insert(array,element)
   array.insert(4,element)
 end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten()
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at (array,integer)
+  array.delete_at(element)
+end
+  
+  
